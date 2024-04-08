2f869ee2b87f72c3d04a071d20964bec2a11fddf
Navneet Yadav <navneetyadavkv2@gmail.com>
2024-04-08 17:12:39 +0530

    First commit

---
+++ TestGit/src/test/GitHub/Add.java
@@ -0,0 +1,27 @@
+package test.GitHub;
+
+import java.util.Scanner;
+
+public class Add {
+	int a,b;
+	
+	public void getData() {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter number a:");
+		a= sc.nextInt();
+		System.out.println("Enter number b:");
+		b = sc.nextInt();
+		
+	}
+	public void putData() {
+		System.out.println("Sum="+(a+b));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Add aa= new Add();
+		aa.getData();
+		aa.putData();
+	}
+
+}
